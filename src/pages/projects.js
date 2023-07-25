15a25b9295fb854fadc56b1f3463d7943a1d37a6
asikhsingh <amritpalasingh@outlook.com>
2023-07-25 03:37:53 -0400

    update projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -11,6 +11,7 @@ import HireMe from '@/components/HireMe'
 
 import project2 from "../../public/images/projects/agency-website-cover-image.jpg";
 import project3 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+import project4 from "../../public/images/projects/stunningportfolio.png";
 import Footer from "@/components/Footer";
 
 const FramerImage = motion(Image);
@@ -61,43 +62,52 @@ const FeaturedProjects = ({ type, title, summary, img, link, github }) => {
         >
           {type}
         </span>
-        <Link
+        
+       
+        <div className="w-full flex items-center justify-between">
+          {/* <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-sm font-semibold 
+            dark:bg-light dark:text-dark
+            sm:px-4 sm:text-base"
+          >
+            VISIT
+          </Link> */}
+          <Link
           href={link}
           target="_blank"
           className="hover:underline undeline-offset-2"
         >
           <h2
-            className="my-2 w-full text-left text-4xl font-bold dark:text-light 
-          sm:text-sm
+            className="my-2 w-full text-left text-3xl font-bold
+          lg:text-2xl md:text-xl sm:text-xl
           "
           >
             {title}
           </h2>
+          
         </Link>
+
+          <Link
+            href={github}
+            target="_blank"
+            className="w-8
+            md:w-6
+            "
+          >
+            {" "}
+            <GithubIcon />{" "}
+          </Link>
+        </div>
         <p
           className="my-2 font-medium text-dark 
         dark:text-light
-        xm:text-sm
+        xm:text-sm sm:text-sm
         "
         >
           {summary}
         </p>
-        <div className="mt-2 flex tems-center">
-          <Link href={github} target="_blank" className="w-10">
-            {" "}
-            <GithubIcon />{" "}
-          </Link>
-          <Link
-            href={link}
-            target="_blank"
-            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-SM font-semibold 
-            dark:bg-light dark:text-dark
-            sm:px-4 sm:text-base
-            "
-          >
-            VISIT PROJECT
-          </Link>
-        </div>
       </div>
     </article>
   );
@@ -107,7 +117,7 @@ const Project = ({ title, type, img, link, github,summary }) => {
   return (
     <article
       className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light
-    xs:p-4
+    xs:p-4 
     "
     >
       <div
@@ -130,38 +140,17 @@ const Project = ({ title, type, img, link, github,summary }) => {
           transition={{ duration: 0.2 }}
         />
       </Link>
-
       <div className="w-full flex flex-col items-start justify-between mt-4">
-        <span
+      <span
           className="text-primary dark:text-primaryDark font-medium text-xl
-        lg:text-lg md:text-base
+        lg:text-lg md:text-base sm:text-sm
         "
         >
           {type}
         </span>
-        <Link
-          href={link}
-          target="_blank"
-          className="hover:underline undeline-offset-2"
-        >
-          <h2
-            className="my-2 w-full text-left text-3xl font-bold
-          lg:text-2xl 
-          "
-          >
-            {title}
-          </h2>
-        </Link>
-<p
-          className="my-2 font-medium text-dark 
-        dark:text-light
-        xm:text-sm
-        "
-        >
-          {summary}
-        </p>
-        <div className="w-full mt-2 flex tems-center justify-between">
-          <Link
+
+      <div className="w-full flex items-center justify-between">
+          {/* <Link
             href={link}
             target="_blank"
             className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-sm font-semibold 
@@ -169,7 +158,21 @@ const Project = ({ title, type, img, link, github,summary }) => {
             sm:px-4 sm:text-base"
           >
             VISIT
-          </Link>
+          </Link> */}
+          <Link
+          href={link}
+          target="_blank"
+          className="hover:underline undeline-offset-2"
+        >
+          <h2
+            className="my-2 w-full text-left text-3xl font-bold
+          lg:text-2xl md:text-xl sm:text-xl
+          "
+          >
+            {title}
+          </h2>
+          
+        </Link>
 
           <Link
             href={github}
@@ -182,10 +185,119 @@ const Project = ({ title, type, img, link, github,summary }) => {
             <GithubIcon />{" "}
           </Link>
         </div>
+        <p
+          className="my-2 font-medium text-dark 
+        dark:text-light
+        xm:text-sm sm:text-sm
+        "
+        >
+          {summary}
+        </p></div>
+    </article>
+  );
+};
+
+const CustomBlock = ({ title, type, img, link, github,summary }) => {
+  return (
+    <article
+      className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light
+    xs:p-4 
+    "
+    >
+      <div
+        className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%]  rounded-[2rem] bg-dark rounded-br-3xl 
+      dark:bg-light
+      md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]
+      "
+      />
+
+      <Link
+        href={link}
+        target="_blank"
+        className="w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <FramerImage
+          src={img}
+          alt={title}
+          className="w-full h-auto "
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.2 }}
+        />
+      </Link>
+
+      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <span
+          className="text-primary dark:text-primaryDark font-medium text-xl
+        lg:text-lg md:text-base sm:text-sm
+        "
+        >
+          {type}
+        </span>
+        {/* <Link
+          href={link}
+          target="_blank"
+          className="hover:underline undeline-offset-2"
+        >
+          <h2
+            className="my-2 w-full text-left text-3xl font-bold
+          lg:text-2xl 
+          "
+          >
+            {title}
+          </h2>
+          
+        </Link> */}
+       
+
+        <div className="w-full flex items-center justify-between">
+          {/* <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-sm font-semibold 
+            dark:bg-light dark:text-dark
+            sm:px-4 sm:text-base"
+          >
+            VISIT
+          </Link> */}
+          <Link
+          href={link}
+          target="_blank"
+          className="hover:underline undeline-offset-2"
+        >
+          <h2
+            className="my-2 w-full text-left text-3xl font-bold
+          lg:text-2xl md:text-xl sm:text-xl
+          "
+          >
+            {title}
+          </h2>
+          
+        </Link>
+
+          <Link
+            href={github}
+            target="_blank"
+            className="w-8
+            md:w-6
+            "
+          >
+            {" "}
+            <GithubIcon />{" "}
+          </Link>
+        </div>
+        <p
+          className="my-2 font-medium text-dark 
+        dark:text-light
+        xm:text-sm sm:text-sm
+        "
+        >
+          {summary}
+        </p>
       </div>
     </article>
   );
 };
+
 
 const Projects = () => {
   return (
@@ -205,28 +317,45 @@ const Projects = () => {
           <AnimatedText
             text="Imagination Trumps Knowledge"
             className="mb-16 
-           lg:!text-6xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
+           lg:!text-6xl sm:mb-8 sm:!text-6xl xs:!text-4xl "
           />
 
           <div
             className="grid grid-cols-12 gap-24 gap-y-32 
-          xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0
+          xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0 sm:gap-y-16
           "
           >
             <div className="col-span-12">
               <FeaturedProjects
                 title="MAP MALL APP"
                 img={project2}
-                summary="The Mall Map app serves as a navigation tool, specifically designed to assist visitors in locating retail stores within a large mall, utilizing their current location. It is tailored to cater to young adults and middle-aged individuals who value efficiency and wish to minimize the time spent navigating through stores."
+                summary="The Mall Map app serves as a navigation tool, specifically designed to assist visitors in locatinwg retail stores within a large mall, utilizing their current location."
                 link="https://amritpalasingh.wixsite.com/home/mall-map-app"
                 github=""
                 type="User Experience"
               />
             </div>
+
+            <div className="col-span-12">
+              <CustomBlock
+                title="STUNNING PORTFOLIO"
+                img={project4}
+                summary="Immerse yourself in the world of creativity as you explore my portfolio, featuring stunningly designed masterpieces that inspire and leave a lasting impression."
+                link="https://amritpalsingh.ca"
+                github="https://github.com/asikhsingh/stunningportfolio"
+                type="Web Design & Developement"
+              />
+            </div>
+
+
+
+
             <div
               className="col-span-6 
             sm:col-span-12"
             >
+
+              
               <Project
                 title="PERILLAS RESTAURANT"
                 img={project3}
